b61ca4c293b9df78e5ff57fbe08845b2009d40c9
Elias Ojala <ojala.s.elias@gmail.com>
2017-06-10 21:00:33 +0000

    Initial commit

---
+++ webview.sh
@@ -0,0 +1,6 @@
+cd /home/theel0ja/github-auto-update/webview/app/src/main/assets/www
+git pull
+cd /home/theel0ja/github-auto-update/webview/
+git add .
+git commit -m "framework7-multi-platform autoupdate"
+git push -u origin master
